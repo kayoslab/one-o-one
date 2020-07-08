438452f64a05c6ace9d64bb33ec8a7006ae029cf
Simon Krüger <hallo@cr0ss.org>
2020-07-08 07:40:08 +0200

    Initial Commit

---
+++ one-o-one/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  one-o-one
+//
+//  Created by cr0ss on 08.07.20.
+//  Copyright © 2020 cr0ss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
